43b276cf23deb987d8b700ab8dfb9f7b219f528b
Spurgeon008 <johnspurgeon820@gmail.com>
2025-08-30 19:16:29 +0530

    message

--- carts/views.py
+++ carts/views.py
@@ -10,7 +10,8 @@ def cart(request, total=0, quantity=0, cart_items=None):
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
-            total += (cart_item.product.price * cart_item.quantity)
+            cart_item.sub_total = cart_item.product.price * cart_item.quantity
+            total += cart_item.sub_total
             quantity += cart_item.quantity
         tax = (2 * total)/100  # 2% tax
         grand_total = total + tax
@@ -58,8 +59,30 @@ def add_cart(request, product_id):
     
     return redirect('cart')
 
+def remove_cart(request, product_id, cart_item_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id, is_active=True)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except CartItem.DoesNotExist:
+        pass
+    return redirect('cart')
+
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
         cart = request.session.create()
     return cart
+
+
+def remove_cart_item(request,product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
